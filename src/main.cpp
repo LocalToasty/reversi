e3624079b065a72f3f6a891f5439c5a2d0acc8ff
Marko van Treeck <markovantreeck@gmail.com>
2016-04-06 14:41:50 +0200

    Feat: winner announcement

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <chrono>
+#include <iostream>
 #include "minimax.hpp"
 #include "reversi.hpp"
 
 int main() {
-  play_reversi(minimax_actor,
-               std::chrono::duration_cast<duration>(std::chrono::minutes(5)),
-               minimax_actor,
-               std::chrono::duration_cast<duration>(std::chrono::minutes(5)),
-               true);
+  // play a game of reversi
+  Player winner = play_reversi(
+      minimax_actor,
+      std::chrono::duration_cast<duration>(std::chrono::minutes(5)),
+      minimax_actor,
+      std::chrono::duration_cast<duration>(std::chrono::minutes(5)), true);
+
+  // announce winner
+  switch (winner) {
+    case Player::dark:
+      std::cout << "Dark wins the game" << std::endl;
+      break;
+
+    case Player::light:
+      std::cout << "Light wins the game" << std::endl;
+      break;
+
+    default:
+      std::cout << "It's a draw" << std::endl;
+      break;
+  }
 }
